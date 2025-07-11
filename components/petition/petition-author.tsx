ebb7779859c0de34e2fe87c05759ec563652defa
Titosy Manankasina <dev.titosy@gmail.com>
2025-07-11 18:03:56 +0300

    feat: refactor PetitionAuthor component for improved layout and styling consistency

--- components/petition/petition-author.tsx
+++ components/petition/petition-author.tsx
@@ -7,14 +7,14 @@ import { Flag, Calendar } from 'lucide-react';
 import { PublicPetition } from '@/schemas/public-petition';
 
 type PetitionAuthorProps = {
-    petition: PublicPetition;
+	petition: PublicPetition;
 };
 
 export function PetitionAuthor({ petition }: PetitionAuthorProps) {
-    return (
+	return (
 		<Card className="shadow-none">
-			<CardContent>
-				<div className="flex flex-col sm:flex-row items-start sm:items-center space-y-3 sm:space-y-0 sm:space-x-4">
+			<CardContent className="flex flex-col gap-6">
+				<div className="flex flex-row items-start sm:items-center sm:space-y-0 space-x-4">
 					<Avatar className="h-10 w-10 sm:h-12 sm:w-12 shrink-0">
 						<AvatarImage src={petition.author.pictureUrl} />
 						<AvatarFallback>
@@ -38,15 +38,15 @@ export function PetitionAuthor({ petition }: PetitionAuthorProps) {
 							</span>
 						</div>
 					</div>
-					<Button
-						variant="outline"
-						size="sm"
-						className="self-end sm:self-auto text-xs sm:text-sm whitespace-nowrap"
-					>
-						<Flag className="h-3 w-3 sm:h-4 sm:w-4 mr-1 sm:mr-2" />
-						Report policy
-					</Button>
 				</div>
+				<Button
+					variant="outline"
+					size="sm"
+					className="w-full md:w-max md:ml-auto self-end sm:self-auto text-xs sm:text-sm whitespace-nowrap"
+				>
+					<Flag className="h-3 w-3 sm:h-4 sm:w-4 mr-1 sm:mr-2" />
+					Report policy
+				</Button>
 			</CardContent>
 		</Card>
 	);
