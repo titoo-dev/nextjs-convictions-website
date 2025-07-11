f7150960f43754862730f1b53cd220b56f99c4c2
Titosy Manankasina <dev.titosy@gmail.com>
2025-07-11 17:00:23 +0300

    feat: add logo image to Logo component

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center">
       <div className="flex items-center space-x-2">
+        <Image 
+          src="/icon.png" 
+          alt="My convictions logo" 
+          width={32} 
+          height={32} 
+          className="h-8 w-8" 
+        />
         <span className="text-xl font-bold text-gray-900">My convictions</span>
       </div>
     </Link>
