51509bf62f90d3b98e0c2bd9b393edfe0adf476e
Titosy Manankasina <dev.titosy@gmail.com>
2025-07-11 15:14:18 +0300

    refactor: simplify DesktopNavigation component by removing unnecessary conditions

--- components/header/desktop-navigation.tsx
+++ components/header/desktop-navigation.tsx
@@ -1,21 +1,12 @@
-'use client';
-
 import { LanguageSelector } from './language-selector';
 import { NavigationButton } from './navigation-button';
-import { usePathname } from 'next/navigation';
-import { RenderWhen } from '../render-when';
 
 export function DesktopNavigation() {
-	const pathname = usePathname();
-	const isNewPage = pathname.includes('new');
-
 	return (
 		<nav className="hidden md:flex items-center space-x-4">
-			<RenderWhen condition={!isNewPage}>
-				<NavigationButton href="/petition/new">
-					Create a petition
-				</NavigationButton>
-			</RenderWhen>
+			<NavigationButton href="/petition/new">
+				Create a petition
+			</NavigationButton>
 			<NavigationButton href="/support-us">Support Us</NavigationButton>
 			<NavigationButton href="/login">Login</NavigationButton>
 			<LanguageSelector />
