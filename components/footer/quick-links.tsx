06104dded6e9d4547d760db3c5d97f552facd9a8
Titosy Manankasina <75468986+titoo-dev@users.noreply.github.com>
2025-07-12 09:35:40 +0300

    feat: enhance internationalization support by adding translations for footer components

--- components/footer/quick-links.tsx
+++ components/footer/quick-links.tsx
@@ -4,23 +4,25 @@ import { FooterSection } from './footer-section';
 import { FooterLink } from './footer-link';
 import { usePathname } from 'next/navigation';
 import { RenderWhen } from '../render-when';
+import { useTranslations } from 'next-intl';
 
 export function QuickLinks() {
 	const pathname = usePathname();
 	const isNewPage = pathname.includes('new');
+	const t = useTranslations('footer.quickLinks');
 
 	return (
-		<FooterSection title="Quick link">
+		<FooterSection title={t('title')}>
 			<li>
-				<FooterLink href="/">Home</FooterLink>
+				<FooterLink href="/">{t('home')}</FooterLink>
 			</li>
 			<RenderWhen condition={!isNewPage}>
 				<li>
-					<FooterLink href="/create">Create a petition</FooterLink>
+					<FooterLink href="/create">{t('createPetition')}</FooterLink>
 				</li>
 			</RenderWhen>
 			<li>
-				<FooterLink href="/support">Support Us</FooterLink>
+				<FooterLink href="/support">{t('supportUs')}</FooterLink>
 			</li>
 		</FooterSection>
 	);
