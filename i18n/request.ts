af03a96c0c5f1f5cd2de159a206401882f63dd25
Titosy Manankasina <75468986+titoo-dev@users.noreply.github.com>
2025-08-05 22:03:41 +0300

    refactor: remove AuthExample component to streamline authentication handling

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,12 +1,19 @@
-import { getRequestConfig } from "next-intl/server";
-import { cookies } from "next/headers";
+import { getRequestConfig } from 'next-intl/server';
+import { cookies } from 'next/headers';
 
 export default getRequestConfig(async () => {
-  const cookie = await cookies();
-  const locale = cookie.get("next-locale")?.value ?? "en";
+	const cookie = await cookies();
 
-  return {
-    locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
-  };
+	const browserLocale = navigator.language.split('-')[0];
+
+	const isBrowserLocaleIncluded = ['en', 'fr', 'es'].includes(browserLocale);
+
+	const locale =
+		cookie.get('next-locale')?.value ??
+		(isBrowserLocaleIncluded ? browserLocale : 'en');
+
+	return {
+		locale,
+		messages: (await import(`../messages/${locale}.json`)).default,
+	};
 });
